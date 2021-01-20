8cb77e2b8e3097ad5d16b7cc31118a88537f90e4
hafiz1412 <57511571+hafiz1412@users.noreply.github.com>
2021-01-20 21:30:20 +0800

    Add files via upload

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"my.example","l":"App"},{"p":"my.example","l":"AppTest"},{"p":"project","l":"MainClass"},{"p":"project","l":"STIW3054_RehatTidur_bot"}];updateSearchResults();
